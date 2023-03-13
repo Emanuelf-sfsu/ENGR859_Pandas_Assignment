f8cbfcfb6b2f4921db6a7e08de14a8ba34881d5f
Emanuel Francis <efrancis1@mail.sfsu.edu>
2023-03-12 18:44:49 -0700

    working on last part

--- Mini_Coding_2_Source_Code/Part2/main.py
+++ Mini_Coding_2_Source_Code/Part2/main.py
@@ -1,57 +1,46 @@
-import tensorflow as tf
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.linear_model import Perceptron
 import matplotlib.pyplot as plt
-
 import tensorflow as tf
+import keras_tuner as kt
+
+"""
+Building model and Fine-Tuning Neural Network Hyperparameters 
+"""
+def build_model(hp):
+    n_hidden = hp.Int("n_hidden", min_value=0, max_value=8, default=2)
+    n_neurons = hp.Int("n_neurons", min_value=16, max_value=256)
+    learning_rate = hp.Float("learning_rate", min_value=1e-4, max_value=1e-2,
+                             sampling="log")
+    optimizer = hp.Choice("optimizer", values=["sgd", "adam"])
+    if optimizer == "sgd":
+        optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
+    else:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    for _ in range(n_hidden):
+        model.add(tf.keras.layers.Dense(n_neurons, activation="relu"))
+    model.add(tf.keras.layers.Dense(10, activation="softmax"))
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer,
+                  metrics=["accuracy"])
+    return model
 
 def main():
     print("Starting ...")
     """
-    Prepare the Dataset 
+    Downloading the fashion MNIST data    
     """
-    # Downloading the fashion MNIST data
     fashion_mnist = tf.keras.datasets.fashion_mnist.load_data()
     (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist
     X_train, y_train = X_train_full[:-5000], y_train_full[:-5000]
     X_valid, y_valid = X_train_full[-5000:], y_train_full[-5000:]
 
-    class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
-                   "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
-    # This code will show you the image.
-    # extra code
 
-    plt.imshow(X_train[0], cmap="binary")
-    plt.axis('off')
-    plt.show()
-
-    """
-    Creating the model using the Sequential API
-    """
-    tf.random.set_seed(42)
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=[28, 28]))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(300, activation="relu"))
-    model.add(tf.keras.layers.Dense(100, activation="relu"))
-    model.add(tf.keras.layers.Dense(10, activation="softmax"))
-
-    print(model.summary())
-
-    model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer="sgd",
-                  metrics=["accuracy"])
-
-
-
-
-
-    """
-    Training and evaluating the model
-    """
-    history = model.fit(X_train, y_train, epochs=30,
-                        validation_data=(X_valid, y_valid))
+    random_search_tuner = kt.RandomSearch(
+        build_model, objective="val_accuracy", max_trials=5, overwrite=True,
+        directory="my_fashion_mnist", project_name="my_rnd_search", seed=42)
+    random_search_tuner.search(X_train, y_train, epochs=10,
+                               validation_data=(X_valid, y_valid))
     return
 
 
